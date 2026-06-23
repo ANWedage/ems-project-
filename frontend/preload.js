f2f33fc2a679d88bc7462bad3ea591cfabc745e6
Adeepa Wedage <IT23826304@my.sliit.lk>
2026-06-23 20:44:56 +0530

    Delete frontend directory

--- frontend/preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('localState', {
-  get: (key) => ipcRenderer.invoke('store:get', key),
-  set: (key, value) => ipcRenderer.invoke('store:set', key, value),
-  delete: (key) => ipcRenderer.invoke('store:delete', key),
-});
